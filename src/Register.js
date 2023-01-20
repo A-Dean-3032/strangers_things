a097a1f331861cced8970611e7f6f7c87e6863e5
Alan Dean <adean3032@gmail.com>
2023-01-20 17:50:06 -0500

    Updated to function + removed extra code

--- src/Register.js
+++ src/Register.js
@@ -6,16 +6,9 @@ const Cohort_Name = "2209-ftb-et-web-pt";
 function Register(props) {
   let passwordReEntry ='';
   let passwordMatch = document.getElementById('passwordMatch');
-  console.log(passwordMatch);
-
-    function matchChecker() {
-      if (passwordMatch.classList.contains('noshow')) {
-        passwordMatch.classList.add('yesshow');
-        passwordMatch.classList.remove('noshow');
-      }
-    }
 
     async function registerHandler () {
+      if (props.password === passwordReEntry) {
       passwordMatch.classList.remove('yesshow');
       passwordMatch.classList.add('noshow');
       const body = JSON.stringify({
@@ -24,8 +17,6 @@ function Register(props) {
           password: props.password,
         },
       })
-
-    // const token = props.token;
     const setToken = props.setToken
     
     try{
@@ -41,48 +32,66 @@ function Register(props) {
 
       const json = await response.json();
       console.log(json);
-      console.log(props.username);
-      console.log(props.password)
       if (json.success) {
         localStorage.setItem('Token_Storage', json.data.token);
         setToken(json.data.token);
-        console.log('Registration successful')
+        console.log('Registration successful');
+        document.getElementById('usernameInput').value='';
+        document.getElementById('passwordInput').value='';
+        document.getElementById('reentryInput').value='';
       } else if (!json.success) {
-        console.log('Failed to register, that username is taken')
+        console.log('Failed to register, that username is taken');
+        document.getElementById('passwordInput').value='';
+        document.getElementById('reentryInput').value='';
       }
     } catch (e) {
       console.log('Unable to register');
       console.error(e);
     }
+  } else if (props.password !== passwordReEntry) {
+    passwordMatch.classList.add('yesshow');
+    passwordMatch.classList.remove('noshow');
   }
-
+    }
 
 
   return (
     <form>
       <div>Username</div>
         <input
+          id = "usernameInput"
           onChange={(e) => {
             props.setUsername(e.target.value);
           }}
+          minLength = "3"
+          required
         ></input>
         <div>Password</div>
         <input
           type={"password"}
+          id = "passwordInput"
           onChange={(e) => {
             props.setPassword(e.target.value);
           }}
+          minLength = "6"
+          required
         ></input>
         <div>Re-enter your password</div>
         <input
           type={"password"}
+          id = "reentryInput"
           onChange={(e) => {
             passwordReEntry = e.target.value;
           }}
+          minLength = "6"
+          required
         ></input>
         <button 
-          onClick={(props.password === passwordReEntry) ? registerHandler : matchChecker 
-          }
+          onClick={(e) => {
+            e.preventDefault();
+            registerHandler();
+          }}
+          id="registerButton"
         >
           Register your account! 
         </button>
